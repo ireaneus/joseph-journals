209f2cd57d4f82f83c66d4a997cedc812400bd6f
ireaneus <bwdave@protonmail.com>
2026-04-10 12:32:28 -0700

    commit md format and series setup

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,4 @@
 
 export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sermons/drafts/EphesiansPt1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsermons%2Fdrafts%2FEphesiansPt1.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sermons/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsermons%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sermons/series/PhilippiansPt1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsermons%2Fseries%2FPhilippiansPt1.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
